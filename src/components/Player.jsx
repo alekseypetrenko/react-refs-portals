5981a92e7962f717bec12576c94a36cebec88b94
alekseypetrenko <menefrego5@gmail.com>
2024-01-19 14:29:12 +0200

    added setting name handlers

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,10 +1,23 @@
+import { useState } from "react";
+
 export default function Player() {
+  const [submitted, setSubmitted] = useState(false);
+  const [enteredPlayername, setEnteredPlayername] = useState("");
+
+  function handlePlayer() {
+    setSubmitted(true);
+  }
+
+  function handleChange(e) {
+    setEnteredPlayername(e.target.value);
+  }
+
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {submitted ? enteredPlayername : "unknown"}</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input type="text" onChange={handleChange} value={enteredPlayername} />
+        <button onClick={handlePlayer}>Set Name</button>
       </p>
     </section>
   );
